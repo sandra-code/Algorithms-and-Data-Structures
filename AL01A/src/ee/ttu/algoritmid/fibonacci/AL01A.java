3e642bd92820e3d036cffc6c5d8af29a2aad1c08
Sandra Kukk <sankuk@ttu.ee>
2019-09-04 14:06:46 +0300

    1. ülesande proov

--- AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -7,8 +7,26 @@ public class AL01A {
      * @param n The number of the sequence to compute.
      * @return The n-th number in Fibonacci series.
      */
-    public String iterativeF(int n) {
-        // TODO
-        return "test";
+    public  String iterativeF(int n) {
+
+        int a = 1;
+        int b = 1;
+        int c;
+
+        for(int i = 3; i<=n; i++){
+            c= a+b;
+            a=b;
+            b=c;
+
+        }
+        String tulemus = String.valueOf(b);
+        return tulemus;
     }
+
+    public static void main(String[] args) {
+        AL01A al01A = new AL01A();
+        System.out.println(al01A.iterativeF(13));
+    }
+
+
 }
