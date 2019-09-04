f8d6974cb91b4ff7e13ad45547b43c43f6c975ee
Sandra Kukk <sankuk@ttu.ee>
2019-09-04 13:24:47 +0300

    Test

---
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -0,0 +1,14 @@
+package ee.ttu.algoritmid.fibonacci;
+
+public class AL01A {
+
+    /**
+     * Compute the Fibonacci sequence number.
+     * @param n The number of the sequence to compute.
+     * @return The n-th number in Fibonacci series.
+     */
+    public String iterativeF(int n) {
+        // TODO
+        return "";
+    }
+}
