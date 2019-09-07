b4938f25402211f4c7643d771ab31dc9e2becdd1
Sandra Kukk <sankuk@ttu.ee>
2019-09-07 14:34:45 +0300

    2. ülesanne test

--- AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -17,7 +17,7 @@ public class AL01B {
         long start = System.currentTimeMillis();
         recursiveF(n);
         long elapsedTime = System.currentTimeMillis()-start;
-        return String.valueOf((TimeUnit.NANOSECONDS.toDays(elapsedTime))/365);
+        return String.valueOf(TimeUnit.MILLISECONDS.toDays(elapsedTime)/365);
     }
 
     /**
@@ -27,7 +27,7 @@ public class AL01B {
      * @return The n-th Fibonacci number as a string.
      */
     public BigInteger recursiveF(int n) {
-        if (n <= 1)
+        if (n <= 2)
             return BigInteger.valueOf(n);
         return recursiveF(n - 1).add(recursiveF(n - 2));
     }
