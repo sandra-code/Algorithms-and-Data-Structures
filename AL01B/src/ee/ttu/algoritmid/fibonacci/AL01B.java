4ce168fcd59290c5f63ac31586b670ce681868bf
Sandra Kukk <sankuk@ttu.ee>
2019-09-07 17:24:03 +0300

    2. ülesanne test

--- AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -1,9 +1,9 @@
 package ee.ttu.algoritmid.fibonacci;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import java.util.Date;
+import java.math.RoundingMode;
+import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
 public class AL01B {
@@ -14,10 +14,22 @@ public class AL01B {
      * @return The time estimate or exact time in YEARS.
      */
     public String timeToComputeRecursiveFibonacci(int n) {
-        long start = System.currentTimeMillis();
-        recursiveF(n);
-        long elapsedTime = System.currentTimeMillis()-start;
-        return String.valueOf((TimeUnit.MILLISECONDS.toDays(elapsedTime))/365);
+
+        /** Calculating the rows with formula: 3 * F(n) - 2 **/
+        BigInteger rows = (recursiveF(n).multiply(new BigInteger("3"))).subtract(new BigInteger("2"));
+
+        /** Getting average time of 100 loops**/
+
+        long start = System.nanoTime();
+        for (int i = 0; i < 100; ++i) {
+            recursiveF(2);
+        }
+        BigDecimal elapsed = (new BigDecimal(System.nanoTime()).subtract(new BigDecimal(start))).divide(new BigDecimal("100"),2, RoundingMode.HALF_UP);
+
+
+        /** Calculating the estimated time **/
+        BigDecimal time = (new BigDecimal(rows).multiply(elapsed)).divide(new BigDecimal("365"),2, RoundingMode.HALF_UP);
+        return String.valueOf(time);
     }
 
     /**
@@ -34,6 +46,6 @@ public class AL01B {
 
     public static void main(String[] args) {
         AL01B al01B = new AL01B();
-        System.out.println(al01B.timeToComputeRecursiveFibonacci(21));
+        System.out.println(al01B.timeToComputeRecursiveFibonacci(5));
     }
 }
