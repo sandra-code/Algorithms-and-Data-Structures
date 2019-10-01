e5bec01b4f03faa1a307d68e451fa89436c1357f
Sandra Kukk <sankuk@ttu.ee>
2019-10-01 21:31:24 +0300

    AL03

---
+++ AL03/src/ee/ttu/algoritmid/popularity/Popularity.java
@@ -0,0 +1,35 @@
+package ee.ttu.algoritmid.popularity;
+
+public class Popularity {
+
+    public Popularity(int maxCoordinates) {
+    }
+
+    /**
+     * @param x, y - coordinates
+     */
+
+    void addPoint(Integer x, Integer y) {
+        // todo
+    }
+
+    /**
+     * @param x, y - coordinates
+     * @return the number of occurrennces of the point
+     */
+
+    int pointPopularity(Integer x,Integer y) {
+        return 0;
+    }
+
+
+
+    /**
+     * @return the number of occurrennces of the most popular point
+     */
+
+    int maxPopularity() {
+        return 0;
+    }
+
+}
