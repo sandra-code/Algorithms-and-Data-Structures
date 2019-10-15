35ff1f1c4040c0201eaccaeb50aa9afb7e95db9e
Sandra Kukk <sankuk@ttu.ee>
2019-10-15 21:26:59 +0300

    AL04 ja HW01

---
+++ AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
@@ -0,0 +1,21 @@
+package ee.ttu.algoritmid.scoreboard;
+
+import java.util.List;
+
+public class ScoreBoard {
+
+
+    /**
+     * Adds a participant's time to the checkpoint scoreboard
+     */
+    public void add(Participant participant) {
+    }
+
+    /**
+     * Returns top n number of participants in the checkpoint to be displayed on the scoreboard
+     * This method will be queried by the tests every time a new participant is added
+     */
+    public List<Participant> get(int n) {
+        return null;
+    }
+}
