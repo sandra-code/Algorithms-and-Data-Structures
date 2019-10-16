ea622168fe4349427003cc6c0468b4a48ca8b15c
Sandra Kukk <sankuk@ttu.ee>
2019-10-16 21:47:16 +0300

    AL04 ja HW01

--- AL04/src/ee/ttu/algoritmid/scoreboard/PartipComparator.java
+++ AL04/src/ee/ttu/algoritmid/scoreboard/PartipComparator.java
@@ -5,10 +5,9 @@ import java.util.Comparator;
  public class PartipComparator implements Comparator<Participant> {
      @Override
      public int compare(Participant p1, Participant p2){
-         if(p1.equals(p2)==true){
+         if (p1.equals(p2)){
              return 0;
          }
-
          return -1;
      }
 
