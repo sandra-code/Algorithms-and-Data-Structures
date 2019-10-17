d69d2c457adb1a63672683cfb24e845385bac802
Sandra Kukk <sankuk@ttu.ee>
2019-10-17 20:51:30 +0300

    AL04

--- AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
+++ AL04/src/ee/ttu/algoritmid/scoreboard/ScoreBoard.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class ScoreBoard {
 
     TreeSet<Participant> set= new TreeSet<>(new PartipComparator());
-    static List<Participant> l = new ArrayList<>();
+    static List<Participant> l = new ArrayList<Participant>();
 
     /**
      * Adds a participant's time to the checkpoint scoreboard
@@ -22,7 +22,7 @@ public class ScoreBoard {
     public List<Participant> get(int n) {
         Iterator<Participant> value = set.iterator();
         int start = 0;
-        while(value.hasNext() && start<n){
+        while(start<n && value.hasNext()){
             Participant p = value.next();
             l.add(p);
             start++;
@@ -30,23 +30,7 @@ public class ScoreBoard {
         return l;
     }
 
-    public static void print(List<Participant> l){
-        List<Participant> list = l;
-        for (int i =0; i<l.size(); i++){
-            System.out.println(l.get(i).getTime() + " "+ l.get(i).getId() );
-        }
-    }
-    public static void main(String[] args) {
-        ScoreBoard sb = new ScoreBoard();
-        sb.add(new Participant(2,"test",20));
-        sb.add(new Participant(3,"tests",34));
-        sb.add(new Participant(1,"tests",34));
-        sb.add(new Participant(56,"tests",19));
-        sb.add(new Participant(7,"tests",19));
-        sb.add(new Participant(80,"tests",19));
-        sb.get(10);
-        print(l);
-    }
+
 }
 
 
