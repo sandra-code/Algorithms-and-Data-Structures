3293fd4ee1525ed92cd80aa14d52c05ce26c0f13
Sandra Kukk <sankuk@ttu.ee>
2019-11-26 08:41:30 +0200

    AL07

--- AL07/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
+++ AL07/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -1,4 +1,11 @@
 package ee.ttu.algoritmid.interestingstamps;
 
+import java.util.List;
+
 public class InterestingStamps {
+
+    public static List<Integer> findStamps(int sum, List<Integer> stampOptions) throws IllegalArgumentException {
+        // TODO
+        return null;
+    }
 }
