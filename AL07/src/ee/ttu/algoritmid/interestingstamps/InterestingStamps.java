4562aef35f56d1c63e9d8f4d4fd5f101e7109881
sankuk <sankuk@ttu.ee>
2019-11-27 13:28:56 +0200

    AL07

--- AL07/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
+++ AL07/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -5,7 +5,12 @@ import java.util.List;
 public class InterestingStamps {
 
     public static List<Integer> findStamps(int sum, List<Integer> stampOptions) throws IllegalArgumentException {
-            //test123
-        return null;
+        List<Integer> finalstamps = stampOptions;
+        if(sum==0){
+            return null;
+        }
+        
+        return finalstamps;
+        //Väljundiks on markide nimiväärtuste list, mille summa on etteantud summa ja kogus minimaalne
     }
 }
