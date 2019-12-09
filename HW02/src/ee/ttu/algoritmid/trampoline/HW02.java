20f2a380f4d151b8ba589bd91c51f682c0d4e616
sankuk <sankuk@ttu.ee>
2019-12-09 12:49:37 +0200

    hw02

---
+++ HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
@@ -0,0 +1,13 @@
+package ee.ttu.algoritmid.trampoline;
+
+import ee.ttu.algoritmid.trampoline.TrampolineCenter;
+
+import java.util.List;
+
+public class HW02 implements TrampolineCenter {
+
+    @Override
+    public List<String> findMinJumps(int[][] map) {
+        return null;
+    }
+}
---
+++ HW02/src/ee/ttu/algoritmid/trampoline/TrampolineCenter.java
@@ -0,0 +1,9 @@
+package ee.ttu.algoritmid.trampoline;
+
+import java.util.List;
+
+public interface TrampolineCenter {
+
+    List<String> findMinJumps(int[][] map);
+
+}
