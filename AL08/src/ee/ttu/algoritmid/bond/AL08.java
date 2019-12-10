1ac343807216f3c36d255d0cadb026b3dc2fa367
sankuk <sankuk@ttu.ee>
2019-12-11 00:41:07 +0200

    al08

--- AL08/src/ee/ttu/algoritmid/bond/AL08.java
+++ AL08/src/ee/ttu/algoritmid/bond/AL08.java
@@ -17,15 +17,22 @@ public class AL08 {
     }
 
     public void talkedToEachOther(String name1, String name2) {
-        //union kasutamine
+        if(memberOfNetwork(name1)==Network.UNKNOWN){
+            name1 = name2;
+        }
+        else if(memberOfNetwork(name2)==Network.UNKNOWN){
+            name2 = name1;
+        }
     }
 
     public void addPerson(String name) {
         if(name.equals("U")){
             //on unfriendly
+           unfriendly(name);
         }
         if(name.equals("A")){
             //on friendly
+            friendly(name);
         }
         else{
             //on unknown
@@ -34,15 +41,17 @@ public class AL08 {
 
     public void friendly(String name) {
         //add to friendly subset
+        disjointSubsets.union("A", name);
+
     }
 
     public void unfriendly(String name) {
         //add to unfriendly subset
+        disjointSubsets.union("U", name);
     }
 
     public Network memberOfNetwork(String name) {
-            return null;
-        /*String root = disjointSubsets.find(name); //tagastab root ehk parenti
+        String root = disjointSubsets.find(name); //tagastab root ehk parenti
         if(root.equals("U")){
             return Network.UNFRIENDLY;
         }
@@ -50,7 +59,7 @@ public class AL08 {
             return  Network.FRIENDLY;
         }
         else{ return Network.UNKNOWN;}
-        */
+
     }
 
 }
