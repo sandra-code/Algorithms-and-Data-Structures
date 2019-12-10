1d6062a5047e52a68cce89439c2c11dfaf1096f2
sankuk <sankuk@ttu.ee>
2019-12-10 20:05:04 +0200

    al08 talkedtoeachother

--- AL08/src/ee/ttu/algoritmid/bond/AL08.java
+++ AL08/src/ee/ttu/algoritmid/bond/AL08.java
@@ -17,7 +17,7 @@ public class AL08 {
     }
 
     public void talkedToEachOther(String name1, String name2) {
-        // TODO
+       disjointSubsets.union(name1, name2);
     }
 
     public void addPerson(String name) {
